4460c9ff3adb82704802ea67ac5772b40718b868
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-03-15 17:33:19 -0400

    Attemp 6

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npm install — only=dev # installs only dev dependencies
-npm install            # installs prod dependencies
-npm run build          # builds the Vue.js app
+npm install --production=false # installs only dev dependencies
+yarn install            # installs prod dependencies
+yarn build          # builds the Vue.js app
