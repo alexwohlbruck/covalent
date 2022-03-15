4103778a19adaf86ce67285acf42e7ec699d1887
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-03-15 17:36:28 -0400

    Attempt 7

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npm install --production=false # installs only dev dependencies
+yarn install --production=false # installs only dev dependencies
 yarn install            # installs prod dependencies
 yarn build          # builds the Vue.js app
